c1c22d9d962bf793658402e59a93198d724b18c6
SCKU@MacBookAir2012 <scku208@gmail.com>
2018-04-07 02:19:02 +0800

    try to upload peegee to pip

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'peegee ',
+    packages = ['peegee'],
+    version = '0.1.0',
+    description = 'A pythonic PostgreSQL client based on psycopg2 suit only for Python3',
+    author = 'scku',
+    author_email = 'scku208@gmail.com',
+    url = 'https://github.com/scku208/peegee',
+    download_url = 'https://github.com/scku208/peegee/archive/0.1.0.tar.gz',
+    keywords = ['postgresql', 'psycopg2', 'database'],
+    classifiers = [],
+)
