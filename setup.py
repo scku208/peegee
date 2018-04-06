013c01f5444bbd92d24b22a6e967023c6f54c73a
SCKU@MacBookAir2012 <scku208@gmail.com>
2018-04-07 02:59:36 +0800

    add "install_requires"

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 setup(
-    name = 'peegee ',
+    name = 'peegee',
     packages = ['peegee'],
     version = '0.1.0',
     description = 'A pythonic PostgreSQL client based on psycopg2 suit only for Python3',
+    install_requires=['psycopg2',],
     author = 'scku',
     author_email = 'scku208@gmail.com',
     url = 'https://github.com/scku208/peegee',
