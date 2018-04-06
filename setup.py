b096a0f170005d6710ccf96f93d0f303a1812315
SCKU@MacBookAir2012 <scku208@gmail.com>
2018-04-07 03:11:57 +0800

    a beautiful mistake

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
     name = 'peegee',
     packages = ['peegee'],
-    version = '0.1.0',
+    version = '0.1.1',
     description = 'A pythonic PostgreSQL client based on psycopg2 suit only for Python3',
     install_requires=['psycopg2',],
     author = 'scku',
     author_email = 'scku208@gmail.com',
     url = 'https://github.com/scku208/peegee',
-    download_url = 'https://github.com/scku208/peegee/archive/0.1.0.tar.gz',
+    download_url = 'https://github.com/scku208/peegee/archive/0.1.1.tar.gz',
     keywords = ['postgresql', 'psycopg2', 'database'],
     classifiers = [],
 )
